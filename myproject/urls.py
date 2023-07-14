0400fef6fec5c7bbd5af5bc923f7dd884824eb14
rukeshshrestha <rukesh.shrestha11@gmail.com>
2023-07-14 08:43:30 +0545

    app create and templates added

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import homepage
+from django.urls import path,include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",homepage)
+    path("",include("crud.urls"))
+    
 ]
