5467fa95ca1c26cb1a22a8d1ce9a8166281c5bb1
rukeshshrestha <rukesh.shrestha11@gmail.com>
2023-08-02 07:48:35 +0545

    Pagination update

--- crud/views.py
+++ crud/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render,redirect
 from .models import Blog,Contact #manager->objects
+from django.core.paginator import Paginator
 
 from .forms import BlogForm
 
 # Create your views here.
 def homepage(request):
     blog=Blog.objects.all()
+    paginator = Paginator(blog, 2)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     data = request.GET.get("search")
+    context = {
+        "blogs":page_obj,
+        "page_obj":page_obj
+    }
     if(data is not "" and data is not None ):
         searchData=Blog.objects.filter(title__contains=data)
         print(searchData)
         return render(request,"crud/index.html",{"blog":searchData})
-
          
 
-    return render(request,"crud/index.html",{"blogs":blog})
+    return render(request,"crud/index.html",context)
 
 def particularData(request,id):
     blog=Blog.objects.get(id=id)
